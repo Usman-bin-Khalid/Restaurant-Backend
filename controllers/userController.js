9c8a840ba2c93f13834ea380236716f6bb7bad7e
Usman <usmanbinkhalidpk@gmail.com>
2026-03-14 22:58:39 +0500

    Middle Ware Issue Solved

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,24 +2,9 @@ const userModel = require("../models/userModel");
 
 // GET USER INFO
 const getUserController = async (req, res) => {
-  try {
+    res.status(200).send("User Data");
+    console.log(req.user._id);
+}
 
-    const user = await userModel.findById(req.userId).select("-password");
-
-    res.status(200).send({
-      success: true,
-      message: "User data fetched successfully",
-      user,
-    });
-
-  } catch (error) {
-    console.log(error);
-    res.status(500).send({
-      success: false,
-      message: "Error in Get User API",
-      error,
-    });
-  }
-};
 
 module.exports = { getUserController };
