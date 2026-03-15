88ae79b4d6ad91017ebd73d24a615d6a6e20f215
Usman <usmanbinkhalidpk@gmail.com>
2026-03-15 14:00:25 +0500

    Get User Profile API Completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,23 @@ const userModel = require("../models/userModel");
 
 // GET USER INFO
 const getUserController = async (req, res) => {
-    res.status(200).send("User Data");
-    console.log(req.user._id);
+    // res.status(200).send("User Data");
+    // console.log(req.user._id);
+    try {
+      // find user
+      const user = await userModel.findById({_id : req.user._id})
+      //validation
+      if (!user) {
+        return res.status(404).send({success : false, message : "User not found"})
+      }
+    //   // hide password
+    //   user.password = undefined;
+      res.status(200).send({success : true, message : 'User data get successfully', user})
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({success : false, message : 'Error in get user API', error});
+    }
+    
 }
 
 
